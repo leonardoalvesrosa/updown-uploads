6c24eb8bdaca1df43b311bddc53d79e91dbedf96
Leonardo Alves Rosa <51093393+leonardoalvesrosa@users.noreply.github.com>
2022-05-22 22:50:06 -0300

    corrections index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,21 +20,22 @@ interface Image {
 }
 
 interface GetImagesResponse {
-  after: string;
-  data: Image[];
+  data: {
+    after: string;
+    data: Image[];
+  }
 }
 
 export default function Home(): JSX.Element {
 
-  const fetchImages = async ({ pageParam = null }): Promise<GetImagesResponse> => {
-    const { data } = await api.get('images', {
-      params: {
-        after: pageParam,
-      },
-    });
-
-    return data;
-  }
+  const fetchImages = async ({ pageParam = 0 }): Promise<GetImagesResponse> =>
+    await api
+      .get(`/api/images`, {
+        params: {
+          after: pageParam,
+        },
+      })
+      .then(res => res);
 
 
   const {
@@ -46,29 +47,14 @@ export default function Home(): JSX.Element {
     isError
   } = useInfiniteQuery(
     'images', fetchImages, {
-    getNextPageParam: lastPage => lastPage?.after || null
+    getNextPageParam: lastPage => lastPage?.data.after || null
   }
   );
 
 
-  // const formattedData = useMemo(() => {
-  //   data?.pages.map(item => item?.data);
-  // }, [data]);
-
-  // let formattedData2 = data?.pages.map(item => item?.data.flat(Infinity));
-  // console.log(formattedData2);
-
-  // const formattedData = useMemo(() => {
-
-  //   const formatted = data?.pages.flatMap(imageData => {
-  //     return imageData.data.flat();
-  //   });
-
-  //   return formatted;
-  // }, [data]);
-
   const formattedData = useMemo(() => {
-    return data?.pages.map(cards => cards.data).flat();
+    return data?.pages.map(({ data: currentData }) =>
+      currentData.data).flat();
   }, [data]);
 
 
